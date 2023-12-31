63c6c33a98114384af9f503a3f5e92f14e4e12e7
emmasonn <words.cs1@gmail.com>
2023-12-31 03:39:43 +0100

    added custom settings font

--- lib/core/icon_fonts/peniremit_font.dart
+++ lib/core/icon_fonts/peniremit_font.dart
@@ -6,8 +6,14 @@ class PeniremitIcon {
   static const _kFontFam = 'Peniremit';
   static const String? _kFontPkg = null;
 
-  static const IconData home = IconData(0xe800, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData wallet = IconData(0xe801, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData analytics = IconData(0xe802, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData homeRounded = IconData(0xe803, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData home =
+      IconData(0xe800, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData wallet =
+      IconData(0xe801, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData analytics =
+      IconData(0xe802, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData homeRounded =
+      IconData(0xe803, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData settings =
+      IconData(0xe804, fontFamily: _kFontFam, fontPackage: _kFontPkg);
 }
